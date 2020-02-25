cc599c8eab6d048ba5df3b5ed1b5e6f589472c08
Jessica J Sun <jessi.slm@hotmail.com>
2020-02-25 21:29:41 +1100

    Router Dom

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
+import { Route, Switch} from 'react-icons'
+
 
 function App() {
   return (
     <>
-    <Home></Home>
-    <Rooms></Rooms>
-    <SingleRoom></SingleRoom>
+    <Route path="/" component={Home}></Route>
+    <Route path="/rooms" component={Rooms}></Route>
+    <Route path="/rooms/:slug" component={SingleRoom}></Route>
     <Error></Error>
     </>
   );
